5b3e85d91f948f99d2d5185cbe81eb94952129d8
Wiktor <wijaweq@gmail.com>
2021-12-01 14:33:10 +0100

    Add part 2 to day 1

--- day01/main.py
+++ day01/main.py
@@ -6,11 +6,24 @@ class Day01(AdventOfCode):
         super().__init__(test)
         self.measurements = super().load_list_from_file(int)
         self.part1()
+        self.part2()
 
     def part1(self):
         counter = 0
         for i in range(1, len(self.measurements)):
             if (self.measurements[i] > self.measurements[i-1]):
                 counter +=1
+
+        super().print_answer(counter)
+
+    def part2(self):
+        last_sum = sum(self.measurements[:3])
+        counter = 0 
+
+        for a, b ,c in zip(self.measurements[1:-2], self.measurements[2:-1], self.measurements[3:]):
+            this_sum = sum([a, b, c])
+            if this_sum > last_sum:
+                counter += 1
+            last_sum = this_sum
 
         super().print_answer(counter)
