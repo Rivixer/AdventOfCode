c9d0ad1d3dacc0fa6b4a1c3bea010f8d4a8113ce
Wiktor <wijaweq@gmail.com>
2022-12-16 21:50:21 +0100

    Add Day15 2022 part 2

--- 2022/day15/day15.py
+++ 2022/day15/day15.py
@@ -1,13 +1,42 @@
 import re
+from dataclasses import dataclass
 
-with open('input.txt') as f:
-    lines = list(map(str.strip, f.readlines()))
 
-result = set()
-for line in lines:
-    x, y, xx, yy = map(int, re.findall(r'\d+', line))
-    dx = abs(xx - x) + abs(yy - y) - abs(2000000 - y)
-    if dx >= 0:
-        result.update(range(x-dx, x+dx+1))
+@dataclass(slots=True)
+class Scanner:
+    x: int
+    y: int
+    dist: int
 
-print(len(result)-1)
+
+if __name__ == '__main__':
+
+    with open('input.txt') as f:
+        lines = list(map(str.strip, f.readlines()))
+
+    scanners: list[Scanner] = list()
+    for line in lines:
+        x, y, xx, yy = map(int, re.findall(r'-*\d+', line))
+        scanners.append(Scanner(x, y, abs(xx-x) + abs(yy-y)))
+
+    # part 1
+    result = set()
+    for scanner in scanners:
+        if (dx := scanner.dist - abs(2_000_000 - scanner.y)) >= 0:
+            result.update(range(scanner.x-dx, scanner.x+dx+1))
+    print(len(result) - 1)
+
+    # part 2
+    for i in range(4_000_000):
+        s: list[tuple[int, int]] = list()
+        for scanner in scanners:
+            if (dx := scanner.dist - abs(i - scanner.y)) >= 0:
+                s.append((scanner.x-dx, scanner.x+dx))
+        s.sort()
+        dxl, dxr = s[0]
+        for ss in s[1:]:
+            dxl2, dxr2 = ss
+            if dxl2 > dxr:
+                print((dxl2 - 1) * 4_000_000 + i)
+                quit()
+            dxr = max(dxr, dxr2)
